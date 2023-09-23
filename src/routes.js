e77ad4b70b457e51461d0683188d21fb0cf75276
dihanto <dihanto2306@gmail.com>
2023-09-23 21:47:40 +0700

    ✨ add crud funtionality

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,35 @@
-const { addBookHandler, getAllBooksHandler } = require("./handler");
+/* eslint-disable linebreak-style */
+/* eslint-disable max-len */
+const {
+  addBookHandler, getAllBooksHandler, getBookByIdHandler, editBookByIdHandler, deleteBookByIdHandler,
+} = require('./handler');
 
 const routes = [
-    {
-        method: 'POST',
-        path: '/books',
-        handler: addBookHandler,
-    },
-    {
-        method: 'GET',
-        path: '/books',
-        handler: getAllBooksHandler,
-    }
+  {
+    method: 'POST',
+    path: '/books',
+    handler: addBookHandler,
+  },
+  {
+    method: 'GET',
+    path: '/books',
+    handler: getAllBooksHandler,
+  },
+  {
+    method: 'GET',
+    path: '/books/{id}',
+    handler: getBookByIdHandler,
+  },
+  {
+    method: 'PUT',
+    path: '/books/{id}',
+    handler: editBookByIdHandler,
+  },
+  {
+    method: 'DELETE',
+    path: '/books/{id}',
+    handler: deleteBookByIdHandler,
+  },
 ];
 
 module.exports = routes;
--- src/server.js
+++ src/server.js
@@ -1,15 +1,17 @@
+/* eslint-disable linebreak-style */
 const Hapi = require('@hapi/hapi');
 const routes = require('./routes');
 
 const init = async () => {
-    const server = Hapi.server({
-        port: 9000,
-        host: 'localhost',
-    });
-    server.route(routes);
+  const server = Hapi.server({
+    port: 9000,
+    host: 'localhost',
+  });
+  server.route(routes);
 
-    await server.start();
-    console.log(`Server berjalan pada ${server.info.uri}`);
-}
+  await server.start();
+  // eslint-disable-next-line no-console
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
 
 init();
